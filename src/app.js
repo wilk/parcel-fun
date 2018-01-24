bbf0fc2f18b6d45b5e94b292029e8355153946ad
Wilk <wilk3ert@gmail.com>
2018-01-24 20:02:32 +0100

    Init repo

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './app.css'
+
+console.log(classes)
+
+export default () => {
+  return (
+    <div className={classes.main}>This is a react app, working!</div>
+  )
+}
